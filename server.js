2f74c1f2d8cb6effd7e9308f493eccb19c011cfa
Matthieu <matthieu.marino@gmail.com>
2016-05-31 16:01:17 +0200

    cleaner version

--- server.js
+++ server.js
@@ -32,17 +32,17 @@ app.get('/', function(req,res){
     res.sendFile('/home/ubuntu/build/index.html');
 });
 
-app.get('/scripts/:name', function(req,res){
-    res.sendFile('/home/ubuntu/build/scripts/'+req.params['name']);
-});
-
-app.get('/styles/:name', function(req,res){
-    res.sendFile('/home/ubuntu/build/styles/'+req.params['name']);
-});
-
-app.get('/fonts/:name', function(req,res){
-    res.sendFile('/home/ubuntu/build/fonts/'+req.params['name']);
-});
+// app.get('/scripts/:name', function(req,res){
+//     res.sendFile('/home/ubuntu/build/scripts/'+req.params['name']);
+// });
+//
+// app.get('/styles/:name', function(req,res){
+//     res.sendFile('/home/ubuntu/build/styles/'+req.params['name']);
+// });
+//
+// app.get('/fonts/:name', function(req,res){
+//     res.sendFile('/home/ubuntu/build/fonts/'+req.params['name']);
+// });
 
 app.get('/ping', function(req, res) {
     res.send("pong");
