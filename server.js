b73023553137be479c8182e03b302a1ea45ad25a
Matthieu <matthieu.marino@gmail.com>
2016-05-31 14:57:41 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+/**
+ * Created by garageposte5 on 18/05/2016.
+ */
+var express = require('express');
+var http = require('http');
+
+var swig = require('swig');
+var swig = new swig.Swig();
+
+
+var list = require('./listFilm.js');
+
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded());     // to support URL-encoded bodies
+
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+//     next();
+// });
+
+//app.use(express.json());       // to support JSON-encoded bodies
+//app.use(express.urlencoded());
+app.set('views', './');
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+
+
+var port = process.env.PORT || '3000';
+app.set('port', port);
+
+app.get('/', function(req,res){
+    res.render('../build/index.html');
+});
+
+app.get('/ping', function(req, res) {
+    res.send("pong");
+});
+
+http.createServer(app).listen(port, function (err) {
+    console.log('listening in http://localhost:' + port);
+});
+
+module.exports = app;
